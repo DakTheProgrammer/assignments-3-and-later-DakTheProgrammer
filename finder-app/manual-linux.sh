6f05d778bd869acd7d81ece2e8027070fc8adf0c
dakota <dakota@sysprog.myguest.virtualbox.org>
2026-01-16 19:15:45 -0600

    finish part 2

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,6 +12,8 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+ROOT_DIR=rootfs
 
 if [ $# -lt 1 ]
 then
@@ -35,46 +37,86 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j4 modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} -j4 dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}/Image
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
+if [ -d "${OUTDIR}/${ROOT_DIR}" ]
 then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
+	echo "Deleting rootfs directory at ${OUTDIR}/${ROOT_DIR} and starting over"
+    sudo rm -rf ${OUTDIR}/${ROOT_DIR}
 fi
 
 # TODO: Create necessary base directories
+mkdir ${ROOT_DIR}
+mkdir -p ${ROOT_DIR}/bin ${ROOT_DIR}/dev ${ROOT_DIR}/etc ${ROOT_DIR}/home ${ROOT_DIR}/lib ${ROOT_DIR}/lib64 ${ROOT_DIR}/proc ${ROOT_DIR}/sbin ${ROOT_DIR}/sys ${ROOT_DIR}/tmp ${ROOT_DIR}/usr ${ROOT_DIR}/var
+mkdir -p ${ROOT_DIR}/usr/bin ${ROOT_DIR}/usr/lib ${ROOT_DIR}/usr/sbin
+mkdir -p ${ROOT_DIR}/var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    echo "clone git busybox"
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
+echo "done clone"
+
 # TODO: Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX="${OUTDIR}/${ROOT_DIR}" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd "${OUTDIR}/rootfs"
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib/.
+cp ${SYSROOT}/lib64/libm.so.6 lib64/.
+cp ${SYSROOT}/lib64/libresolv.so.2 lib64/.
+cp ${SYSROOT}/lib64/libc.so.6 lib64/.
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+cd "$FINDER_APP_DIR"
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE}
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp autorun-qemu.sh ${OUTDIR}/rootfs/home
+cp finder-test.sh ${OUTDIR}/rootfs/home
+cp finder.sh ${OUTDIR}/rootfs/home
+cp writer ${OUTDIR}/rootfs/home
+
+mkdir -p ${OUTDIR}/rootfs/home/conf
+cp ../conf/assignment.txt ${OUTDIR}/rootfs/home/conf/assignment.txt 
+cp ../conf/username.txt ${OUTDIR}/rootfs/home/conf/username.txt 
 
 # TODO: Chown the root directory
+cd "${OUTDIR}/rootfs"
+sudo chown -R root:root *
 
 # TODO: Create initramfs.cpio.gz
+cd "${OUTDIR}/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
